fdb97a0739aa2b5b85f3824dc43b311b42c12a20
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-04-18 13:52:53 -0300

    Criando estrutura inicial do projeto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return <Text>App Start</Text>;
+  }
+}
+
+const styles = StyleSheet.create({});
